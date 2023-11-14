068f206f3680d964aeae3a79d0efc2bc86ebec42
Jonah Zabel <develop.polony@gmx.net>
2023-11-14 20:10:00 +0100

    Add bootstrap bash script

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+ln ./config/zshrc ~/.zshrc
+ln ./config/p10k ~/.p10k.zsh
+
+ln ./config/gtk-2 ~/.gtkrc-2.0
+ln ./config/gtk-3 ~/.config/gtk-3.0
+ln ./config/qt ~/.config/Trolltech.conf
+
+mkdir ~/.config/i3
+mkdir ~/.config/picom
+mkdir ~/.config/polybar
+mkdir ~/.config/rofi
+
+ln ./config/i3 ~/.config/i3/config 
+ln ./config/picom ~/.config/picom/picom.conf
+ln ./config/polybar ~/.config/polybar/config.ini
+ln ./config/rofi ~/.config/rofi/config.rasi
+ln ./theme/rofi ~/.config/rofi/CustomTheme.conf
+
+mkdir ~/.config/kitty
+
+ln ./config/kitty ~/.config/kitty/kitty.conf
+ln ./theme/kitty ~/.config/kitty/current-theme.conf
