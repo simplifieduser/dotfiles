1f5aa3a55831bf99243aa9382ae5e52f7effc604
Jonah Zabel <develop.polony@gmx.net>
2024-01-13 23:38:53 +0100

    Updated setup script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,50 @@
 #!/usr/bin/bash
 
+ln ./config/zshrc    ~/.zshrc
+ln ./config/xprofile ~/.xprofile
+ln ./config/p10k     ~/.p10k.zsh
+ln ./config/gtk-2    ~/.gtkrc-2.0
+
 mkdir ~/.config
 
-ln ./config/zshrc ~/.zshrc
-ln ./config/p10k ~/.p10k.zsh
-
-ln ./config/gtk-2 ~/.gtkrc-2.0
-ln ./config/gtk-3 ~/.config/gtk-3.0/settings.ini
 ln ./config/qt ~/.config/Trolltech.conf
 
-mkdir ~/.config/i3
-mkdir ~/.config/picom
-mkdir ~/.config/polybar
-mkdir ~/.config/rofi
 
-ln ./config/xsession ~/.xsession
+mkdir ~/.config/i3
 ln ./config/i3 ~/.config/i3/config 
+
+mkdir ~/.config/picom
 ln ./config/picom ~/.config/picom/picom.conf
+
+mkdir ~/.config/polybar
 ln ./config/polybar ~/.config/polybar/config.ini
+
+mkdir ~/.config/rofi
 ln ./config/rofi ~/.config/rofi/config.rasi
-ln ./theme/rofi ~/.config/rofi/CustomTheme.rasi
+ln ./theme/rofi  ~/.config/rofi/CustomTheme.rasi
 
 mkdir ~/.config/kitty
-
 ln ./config/kitty ~/.config/kitty/kitty.conf
-ln ./theme/kitty ~/.config/kitty/current-theme.conf
+ln ./theme/kitty  ~/.config/kitty/current-theme.conf
+
+mkdir ~/.config/gtk-3.0
+ln ./config/gtk-3  ~/.config/gtk-3.0/settings.ini
+
+mkdir ~/.config/pulse
+ln ./config/pulseaudio ~/.config/pulse/default.pa
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
